825c4d2e038100b0a9e59cbb684bb65728f26812
yushijinhun <yushijinhun@gmail.com>
2017-12-02 19:25:00 +0800

    add interfaces for gui login

---
+++ src/main/java/org/to2mbn/authlibinjector/login/LoginApplication.java
@@ -0,0 +1,24 @@
+package org.to2mbn.authlibinjector.login;
+
+import static java.util.Optional.empty;
+import java.awt.GraphicsEnvironment;
+import java.util.Optional;
+import org.to2mbn.authlibinjector.InjectorConfig;
+
+public class LoginApplication {
+
+	public static boolean isClientSide() {
+		if (GraphicsEnvironment.isHeadless())
+			return false;
+
+		// TODO: maybe we'd better have more methods to test if it's server/client side?
+
+		return true;
+	}
+
+	public static Optional<InjectorConfig> launchAndBlock() {
+		// TODO
+		return empty();
+	}
+
+}
